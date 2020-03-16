4e9a118cafe079a56afc5b2dfe26d5b536b2241c
Saada2016 <saad.alikhan@cee-solutions.com>
2020-03-15 22:53:25 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import About from './component/About';
+import Work from './component/Work';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import 'font-awesome/css/font-awesome.min.css';
-// import MainContent from './component/MainContent';
-// import Footer from './component/Footer';
 
 function App() {
   return (
     <div className="App">   
       <Header/>
+      <About/>
+      <br/>
+      <Work/>
       <br/>
       <Footer/>
       
